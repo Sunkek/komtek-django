5a60c82e552df8c42edf32ddc2c58e9bd6b93439
“Suncake” <xexc@mail.ru>
2020-10-13 17:25:56 +0500

    Add debug print

--- komtek-api/komtek/komtek/api/exceptions.py
+++ komtek-api/komtek/komtek/api/exceptions.py
@@ -1,4 +1,5 @@
 from rest_framework.views import exception_handler
+from django.db.models.Model import DoesNotExist
 
 def custom_exception_handler(exc, context):
     """Custom handler to make more verbose error responses"""
@@ -6,8 +7,7 @@ def custom_exception_handler(exc, context):
     # to get the standard error response.
     response = exception_handler(exc, context)
     print("EXCEPTION")
-    print(exc.detail)
-    print(exc.get_codes())
-    print(exc.get_full_details())
+    if isinstance(exc, DoesNotExist):
+        print(exc.__dict__)
 
     return response
