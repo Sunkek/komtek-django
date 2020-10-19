279dbd56f0ebbc1864b75cc88d2815c17c5e1386
“Suncake” <xexc@mail.ru>
2020-10-19 10:24:06 +0500

    Add populate catalog method

--- komtek-api/komtek/komtek/api/admin.py
+++ komtek-api/komtek/komtek/api/admin.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
+from random import choice, randint
+from django.contrib import admin, messages
 from .models import Catalog, Element
 from .utils import format_version
+
+LETTERS = list("qwertyuiopasdfghjklzxcvbnm")
 
 @admin.register(Catalog)
 class CatalogAdmin(admin.ModelAdmin):
@@ -9,6 +12,25 @@ class CatalogAdmin(admin.ModelAdmin):
         """Overriding Catalog's save model function to format its version"""
         obj.version = format_version(obj.version)
         super().save_model(request, obj, form, change)
+
+    def populate(self, request, queryset):
+        for catalog in queryset:
+            try:
+                for i in range(10):
+                    element = Element(
+                        catalog=catalog,
+                        code=f"{randint(0,9)}{choice(LETTERS)}{choice(LETTERS)}",
+                        description="Автоматически сгенерированный элемент",
+                    )
+                    element.save()
+            except Exception as e:
+                print(e)
+        self.message_user(
+            request, 
+            "Элементы добавлены", 
+            messages.SUCCESS
+        )
+    populate.short_description = "Добавить 10 случайных элементов"
 
 
 admin.site.register(Element)
