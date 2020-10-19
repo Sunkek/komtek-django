c8269764650059fa82d55e95402ece4f6551bf34
“Suncake” <xexc@mail.ru>
2020-10-19 11:51:32 +0500

    Remove form

--- komtek-api/komtek/komtek/api/admin.py
+++ komtek-api/komtek/komtek/api/admin.py
@@ -12,14 +12,6 @@ from .utils import format_version
 LETTERS = list("QWERTYUIOPASDFGHJKLZXCVBNM")
 
 
-class NumberOfElementsForm(forms.Form):
-    number = forms.IntegerField(
-        required=True,
-        label="Количество: ",
-        initial=10,
-    )
-
-
 @admin.register(Catalog)
 class CatalogAdmin(admin.ModelAdmin):
     actions = ["populate"]
@@ -32,35 +24,30 @@ class CatalogAdmin(admin.ModelAdmin):
     def populate(self, request, queryset):
         """Add 10 randomly created elements to the selected catalogs.
         If an element already exists - update its creation date."""
-        try:
-            form = NumberOfElementsForm(request.POST)
-            for catalog in queryset:
-                for i in range(form.cleaned_data["number"]):
-                    code = f"{randint(0,9)}{choice(LETTERS)}{choice(LETTERS)}"
-                    element = Element(
+        for catalog in queryset:
+            for i in range(10):
+                code = f"{randint(0,9)}{choice(LETTERS)}{choice(LETTERS)}"
+                element = Element(
+                    catalog=catalog,
+                    code=code,
+                    description="Автоматически сгенерированный элемент",
+                )
+                try:
+                    element.save()
+                except IntegrityError:
+                    element = Element.objects.get(
                         catalog=catalog,
                         code=code,
                         description="Автоматически сгенерированный элемент",
                     )
-                    try:
-                        element.save()
-                    except IntegrityError:
-                        element = Element.objects.get(
-                            catalog=catalog,
-                            code=code,
-                            description="Автоматически сгенерированный элемент",
-                        )
-                        element.date_created = dt_date.today()
-                        element.save()
-            self.message_user(
-                request, 
-                "Элементы добавлены", 
-                messages.SUCCESS
-            )
-        except Exception as e:
-            print(e)
-            print(type(e))
-    populate.short_description = "Добавить N случайных элементов"
+                    element.date_created = dt_date.today()
+                    element.save()
+        self.message_user(
+            request, 
+            "Элементы добавлены", 
+            messages.SUCCESS
+        )
+    populate.short_description = "Добавить 10 случайных элементов"
 
 
 admin.site.register(Element)
